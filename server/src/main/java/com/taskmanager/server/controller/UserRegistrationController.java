ab58f17afa72e77021f337d14c265801b53200ca
Preston Beachum <prestonbeachum@gmail.com>
2025-05-27 21:12:02 -0400

    Added UserRegistrationController file.

---
+++ server/src/main/java/com/taskmanager/server/controller/UserRegistrationController.java
@@ -0,0 +1,33 @@
+package com.taskmanager.server.controller;
+
+import com.taskmanager.server.model.User;
+import com.taskmanager.server.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/register")
+public class RegistrationController {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public RegistrationController(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> registerUser(@RequestBody User user) {
+        if (userRepository.findByUsername(user.getUsername()).isPresent()) {
+            return ResponseEntity.badRequest().body("Username already exists");
+        }
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+        return ResponseEntity.ok("User registered successfully");
+    }
+}
